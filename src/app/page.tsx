53c215160ca57ed30942ae2ae6d8ab54250de18f
fo-ji <fumiya1753@gmail.com>
2022-12-27 11:54:55 +0900

    data fetch ssg, ssr, isr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { NotesList } from './components';
+
 const Page = () => (
   <main>
-    <div className="m-10 text-center">Hello World</div>
+    <div className="m-10 text-center">
+      <p>Hello World</p>
+      {/* 現状、β版で公式の対応が間に合ってないので、強引に回避 */}
+      {/* @ts-ignore */}
+      <NotesList />
+    </div>
   </main>
 );
 
